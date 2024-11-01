1eabf07184c695f56a68836e48f24188c2fded1c
Roger Cheng <roger.random@outlook.com>
2024-10-31 18:10:47 -0700

    Fastener shelf organizer 15 x 45 x 75

--- single_tray.py
+++ single_tray.py
@@ -3,12 +3,12 @@ import cadquery as cq
 
 from dovetailstoragegrid import DovetailStorageGrid as dsg
 
-cell_size = 32 #mm
+cell_size = 15 #mm
 
-dsg_01 = dsg(x = cell_size, y = cell_size, z = 62, dovetail_gap=0.1)
+dsg_01 = dsg(x = cell_size, y = cell_size, z = 75, dovetail_angle=60, dovetail_protrusion = 2.5, dovetail_gap=0.2)
 
-show_object(dsg_01.label_tray(1,1), options = {"alpha":0.5, "color":"blue"})
+show_object(dsg_01.label_tray(2,1), options = {"alpha":0.5, "color":"blue"})
 
-show_object(dsg_01.label_tray(1,1).translate((cell_size,0,0)), options = {"alpha":0.5, "color":"yellow"})
+show_object(dsg_01.label_tray(2,1).translate((cell_size*2,0,0)), options = {"alpha":0.5, "color":"yellow"})
 
-show_object(dsg_01.label_tray(1,1).translate((0,cell_size,0)), options = {"alpha":0.5, "color":"red"})
+show_object(dsg_01.label_tray(2,1).translate((0,cell_size,0)), options = {"alpha":0.5, "color":"red"})
