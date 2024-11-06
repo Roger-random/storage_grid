ea6bba13be355458ca4921187dd221de8d00ed14
Roger Cheng <roger.random@outlook.com>
2024-11-06 12:23:38 -0800

    MIT license and specifying CQ-Editor

--- for_cq-editor.py
+++ for_cq-editor.py
@@ -1,4 +1,35 @@
-import math
+"""
+MIT License
+
+Copyright (c) 2024 Roger Cheng
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+"""
+Using CQ-Editor to generate dovetail storage grid trays.
+
+This file is intended to be opened in CQ-Editor for interactive visualization
+while experimenting with different parameters. See dovetailstoragegrid.py for
+documentation on what each parameter means.
+"""
+
 import cadquery as cq
 
 from dovetailstoragegrid import DovetailStorageGrid as dsg
