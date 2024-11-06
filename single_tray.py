c5fa5c00bbd6216e668580e9e697f9aa06d4fb08
Roger Cheng <roger.random@outlook.com>
2024-11-06 12:14:20 -0800

    Three adjacent trays to visualize tray+dovtail gaps.

--- single_tray.py
+++ single_tray.py
@@ -5,10 +5,15 @@ from dovetailstoragegrid import DovetailStorageGrid as dsg
 
 cell_size = 15 #mm
 
-dsg_01 = dsg(x = cell_size, y = cell_size, z = 75, dovetail_angle=60, dovetail_protrusion = 2.5, dovetail_gap=0.2)
+dsg_01 = dsg(x = cell_size, y = cell_size, z = 75)
 
-show_object(dsg_01.label_tray(2,1), options = {"alpha":0.5, "color":"blue"})
+tray_x = 2
+tray_y = 1
 
-show_object(dsg_01.label_tray(2,1).translate((cell_size*2,0,0)), options = {"alpha":0.5, "color":"yellow"})
+tray = dsg_01.label_tray(tray_x, tray_y)
 
-show_object(dsg_01.label_tray(2,1).translate((0,cell_size,0)), options = {"alpha":0.5, "color":"red"})
+show_object(tray, options = {"alpha":0.5, "color":"blue"})
+
+show_object(tray.translate((cell_size*tray_x,0,0)), options = {"alpha":0.5, "color":"yellow"})
+
+show_object(tray.translate((0,cell_size*tray_y,0)), options = {"alpha":0.5, "color":"red"})
